4958bb3986f8e98bba756ea177ced04d0590c348
stepGT <stepgt@mail.ru>
2020-07-11 22:51:19 +1000

    Added mysql db connect and query get marks

---
+++ backend/db_conn.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect(HOST, USER, PASS, DB_NAME);
+mysqli_query($conn, "SET NAMES utf8");
---
+++ backend/db_const.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ *
+ */
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'rucar';
