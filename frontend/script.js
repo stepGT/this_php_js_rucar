bfcc8593fbf07d62bc3755c21bbb841f19764f77
stepGT <stepgt@mail.ru>
2020-07-14 22:22:19 +1000

    Refactor. Create post query function

--- frontend/script.js
+++ frontend/script.js
@@ -1,69 +1,46 @@
 $(document).ready(function () {
   // https://auto.basebuy.ru
-  let rucar_mark = $('#this_rucar_mark');
-  let rucar_model = $('#this_rucar_model');
+  let rucar_mark       = $('#this_rucar_mark');
+  let rucar_model      = $('#this_rucar_model');
   let rucar_generation = $('#this_rucar_generation');
-  let rucar_serie = $('#this_rucar_serie');
+  let rucar_serie      = $('#this_rucar_serie');
+  //
+  const _get_post = (entity, selector, value = '') => {
+    //
+    if (entity !== 'mark') {
+      selector.children().remove();
+      selector.attr('disabled', true);
+    }
+    //
+    $.post('/backend/_get_' + entity + '.php', {value: value})
+        .done(function (response) {
+          let jsonData = JSON.parse(response);
+          let html = '';
+          //
+          $.each(jsonData, function (key, value) {
+            let _entity_id = 'id_car_' + entity;
+            html += '<option value="' + value[_entity_id] + '">' + value.name + '</option>';
+          });
+          selector.append(html);
+          selector.attr('disabled', false).append(html);
+        });
+  };
 
   // Get all mark
-  $.post('/backend/_get_mark.php', function (response) {
-    let jsonData = JSON.parse(response);
-    let html = '';
-    //
-    $.each(jsonData, function (key, value) {
-      html += '<option value="' + value.id_car_mark + '">' + value.name + '</option>';
-    });
-    rucar_mark.append(html);
-  });
+  _get_post('mark', rucar_mark);
 
   // Get models
   rucar_mark.change(function () {
-    rucar_model.attr('disabled', true);
-    // Clear old model data
-    rucar_model.children().remove();
-    //
-    $.post('/backend/_get_model.php', {value: $(this).val()})
-        .done(function (response) {
-          let jsonData = JSON.parse(response);
-          let html = '<option value="0">Выберите модель</option>';
-          $.each(jsonData, function (key, value) {
-            html += '<option value="' + value.id_car_model + '">' + value.name + '</option>';
-          });
-          rucar_model.attr('disabled', false).append(html);
-        });
+    _get_post('model', rucar_model, $(this).val());
   });
 
   // Get generation
   rucar_model.change(function () {
-    rucar_generation.attr('disabled', true);
-    // Clear old model data
-    rucar_generation.children().remove();
-    //
-    $.post('/backend/_get_generation.php', {value: $(this).val()})
-        .done(function (response) {
-          let jsonData = JSON.parse(response);
-          let html = '<option value="0">Выберите поколение</option>';
-          $.each(jsonData, function (key, value) {
-            html += '<option value="' + value.id_car_generation + '">' + value.name + '</option>';
-          });
-          rucar_generation.attr('disabled', false).append(html);
-        });
+    _get_post('generation', rucar_generation, $(this).val());
   });
 
   // Get serie
   rucar_generation.change(function () {
-    rucar_serie.attr('disabled', true);
-    // Clear old model data
-    rucar_serie.children().remove();
-    //
-    $.post('/backend/_get_serie.php', {value: $(this).val()})
-        .done(function (response) {
-          let jsonData = JSON.parse(response);
-          let html = '<option value="0">Выберите серию</option>';
-          $.each(jsonData, function (key, value) {
-            html += '<option value="' + value.id_car_serie + '">' + value.name + '</option>';
-          });
-          rucar_serie.attr('disabled', false).append(html);
-        });
+    _get_post('serie', rucar_serie, $(this).val());
   });
 });
