382877e3826af981bcc0b91fb9275e61920bad00
stepGT <stepgt@mail.ru>
2020-07-26 13:15:03 +1000

    Refactor backend

--- backend/_get_query.php
+++ backend/_get_query.php
@@ -2,31 +2,36 @@
 require_once 'db_const.php';
 require_once 'db_conn.php';
 //
-$entity   = htmlspecialchars($_POST['entity']);
-$value    = htmlspecialchars($_POST['value']);
-$prev_ent = htmlspecialchars($_POST['prev_ent']);
+$entity = 'car_mark';
 $entities = [];
 //
 switch ($entity) {
-  case 'mark':
-    $query = mysqli_query($conn, 'SELECT * FROM ' . DB_NAME . '.`car_' . $entity . '` WHERE `name` IS NOT NULL');
+  case 'car_mark':
+    $STH = $DBH->prepare("SELECT * FROM car_mark");
+    $STH->execute([$entity]);
+    $row = $STH->fetch();
+    print '<pre>';
+    print_r($row);
+    print '</pre>';
+    exit;
     break;
-  case 'generation':
+  /*case 'generation':
     $query = mysqli_query($conn, 'SELECT * FROM ' . DB_NAME . '.`car_' . $entity . '` WHERE `id_car_' . $prev_ent . '` = ' . $value . ' AND `year_begin` IS NOT NULL AND `year_end` IS NOT NULL');
     break;
   default:
-    $query = mysqli_query($conn, 'SELECT * FROM ' . DB_NAME . '.`car_' . $entity . '` WHERE `id_car_' . $prev_ent . '` = ' . $value);
+    $query = mysqli_query($conn, 'SELECT * FROM ' . DB_NAME . '.`car_' . $entity . '` WHERE `id_car_' . $prev_ent . '` = ' . $value);*/
 }
 //
-while ($item = mysqli_fetch_array($query)) {
-  $entities[$item['id_car_' . $entity]]['name'] = $item['name'];
-  $entities[$item['id_car_' . $entity]]['id_car_' . $entity] = $item['id_car_' . $entity . ''];
+/*while ($row = $STH->fetchAll()) {
+  $entities[$row['id_car_' . $entity]]['name'] = $row['name'];
+  $entities[$row['id_car_' . $entity]]['id_car_' . $entity] = $row['id_car_' . $entity . ''];
   //
   if ($entity === 'generation') {
-    $entities[$item['id_car_' . $entity]]['year_begin'] = $item['year_begin'];
-    $entities[$item['id_car_' . $entity]]['year_end'] = $item['year_end'];
+    $entities[$row['id_car_' . $entity]]['year_begin'] = $row['year_begin'];
+    $entities[$row['id_car_' . $entity]]['year_end'] = $row['year_end'];
   }
-}
-mysqli_close($conn);
+}*/
+// Close connect
+$DBH = NULL;
 // Send a JSON encoded object to client
 echo json_encode($entities);
