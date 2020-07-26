82c4bc3df0d1a0dd8560c37fcba12bb9b85b2c69
stepGT <stepgt@mail.ru>
2020-07-26 13:14:47 +1000

    Added PDO

--- backend/db_conn.php
+++ backend/db_conn.php
@@ -1,3 +1,13 @@
 <?php
-$conn = mysqli_connect(HOST, USER, PASS, DB_NAME);
-mysqli_query($conn, "SET NAMES utf8");
+//
+try {
+  $options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Exceptions
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  ];
+  // Database Handle
+  $DBH = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME . "", USER, PASS, $options);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
+}
